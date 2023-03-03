5d5214c0d4d2e86e6328a7ddde8aa058fa89d89c
Noushadozi <noushadozi333@gmail.com>
2023-03-03 15:46:03 +0600

    done

--- review.js
+++ review.js
@@ -9,6 +9,23 @@ document.getElementById('review-btn').addEventListener('click', function(){
     parent.appendChild(h1);
     myElement.value = '';
 })
+var input = document.getElementById("floatingTextarea2");
+input.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    const myElement = document.getElementById("floatingTextarea2");
+    const myElementValue = myElement.value;
+
+    //   console.log(myElementValue);
+
+    const p = document.createElement("p");
+    p.innerText = myElementValue;
+
+    const parent = document.querySelector("#commnet-container");
+    parent.appendChild(p);
+    document.getElementById("floatingTextarea2").value = "";
+  }
+});
 // document.getElementById('review-btn').addEventListener('click', function(){
 //     const myElement = document.getElementById('floatingTextarea2');
 //     const myElementValue = myElement.value;

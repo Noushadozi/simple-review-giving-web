ae779dd6bbaa93a44c1ab55639fc5bfc6c928593
Noushadozi <noushadozi333@gmail.com>
2023-03-03 15:40:34 +0600

    first commit

---
+++ review.js
@@ -0,0 +1,132 @@
+document.getElementById('review-btn').addEventListener('click', function(){
+    const myElement = document.getElementById('floatingTextarea2');
+    const myElementValue = myElement.value;
+
+    const h1 = document.createElement('h1');
+    h1.innerText = myElementValue;
+
+    const parent = document.getElementById('commnet-container');
+    parent.appendChild(h1);
+    myElement.value = '';
+})
+// document.getElementById('review-btn').addEventListener('click', function(){
+//     const myElement = document.getElementById('floatingTextarea2');
+//     const myElementValue = myElement.value;
+
+//     const h1 = document.createElement('h1');
+//     h1.innerText = myElementValue;
+
+//     const parent = document.getElementById('commnet-container');
+//     parent.appendChild(h1);
+//     myElement.value = '';
+// })
+// document.getElementById('review-btn').addEventListener('click', function(){
+//     const myElement = document.getElementById('floatingTextarea2');
+//     const myElementValue = myElement.value;
+
+//     const h1 = document.createElement('h1');
+//     h1.innerText = myElementValue;
+
+//     const parent = document.getElementById('commnet-container');
+//     parent.appendChild(h1);
+//     myElement.value = '';
+// })
+// document.getElementById('review-btn').addEventListener('click', function(){
+//     const myElement = document.getElementById('floatingTextarea2');
+//     const myElementValue = myElement.value;
+
+//     const h1 = document.createElement('h1');
+//     h1.innerText = myElementValue;
+
+//     const parent = document.getElementById('commnet-container');
+//     parent.appendChild(h1);
+//     myElement.value = '';
+// })
+// document.getElementById('review-btn').addEventListener('click', function(){
+//     const myElement = document.getElementById('floatingTextarea2');
+//     const myElementValue = myElement.value;
+
+//     const h1 = document.createElement('h1');
+//     h1.innerText = myElementValue;
+
+//     const parent = document.getElementById('commnet-container');
+//     parent.appendChild(h1);
+//     myElement.value = '';
+// })
+// document.getElementById('review-btn').addEventListener('click', function(){
+//     const myElement = document.getElementById('floatingTextarea2');
+//     const myElementValue = myElement.value;
+
+//     const h1 = document.createElement('h1');
+//     h1.innerText = myElementValue;
+
+//     const parent = document.getElementById('commnet-container');
+//     parent.appendChild(h1);
+//     myElement.value = '';
+// })
+// document.getElementById('review-btn').addEventListener('click', function(){
+//     const myElement = document.getElementById('floatingTextarea2');
+//     const myElementValue = myElement.value;
+
+//     const h1 = document.createElement('h1');
+//     h1.innerText = myElementValue;
+
+//     const parent = document.getElementById('commnet-container');
+//     parent.appendChild(h1);
+//     myElement.value = '';
+// })
+// document.getElementById('review-btn').addEventListener('click', function(){
+//     const myElement = document.getElementById('floatingTextarea2');
+//     const myElementValue = myElement.value;
+
+//     const h1 = document.createElement('h1');
+//     h1.innerText = myElementValue;
+
+//     const parent = document.getElementById('commnet-container');
+//     parent.appendChild(h1);
+//     myElement.value = '';
+// })
+// document.getElementById('review-btn').addEventListener('click', function(){
+//     const myElement = document.getElementById('floatingTextarea2');
+//     const myElementValue = myElement.value;
+
+//     const h1 = document.createElement('h1');
+//     h1.innerText = myElementValue;
+
+//     const parent = document.getElementById('commnet-container');
+//     parent.appendChild(h1);
+//     myElement.value = '';
+// })
+// document.getElementById('review-btn').addEventListener('click', function(){
+//     const myElement = document.getElementById('floatingTextarea2');
+//     const myElementValue = myElement.value;
+
+//     const h1 = document.createElement('h1');
+//     h1.innerText = myElementValue;
+
+//     const parent = document.getElementById('commnet-container');
+//     parent.appendChild(h1);
+//     myElement.value = '';
+// })
+// document.getElementById('review-btn').addEventListener('click', function(){
+//     const myElement = document.getElementById('floatingTextarea2');
+//     const myElementValue = myElement.value;
+
+//     const h1 = document.createElement('h1');
+//     h1.innerText = myElementValue;
+
+//     const parent = document.getElementById('commnet-container');
+//     parent.appendChild(h1);
+//     myElement.value = '';
+// })
+// document.getElementById('review-btn').addEventListener('click', function(){
+//     const myElement = document.getElementById('floatingTextarea2');
+//     const myElementValue = myElement.value;
+
+//     const h1 = document.createElement('h1');
+//     h1.innerText = myElementValue;
+
+//     const parent = document.getElementById('commnet-container');
+//     parent.appendChild(h1);
+//     myElement.value = '';
+// })
